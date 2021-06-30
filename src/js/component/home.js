bd151302ae7447d4db3de58ceab2c9011dc4a2de
jjmorales <jjmorales879@gmail.com>
2021-06-30 19:54:03 -0400

    to-do

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,48 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+export class Home extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { tasks: ["hacer cafe", "fubol", "cocinar"], newtask: "" };
+		this.handleAddTask = this.handleAddTask.bind(this);
+		this.handleChangeTask = this.handleChangeTask.bind(this);
+	}
+
+	handleChangeTask(event) {
+		this.setState({
+			...this.state.tasks,
+			newtask: event.target.value
+		});
+	}
+
+	handleAddTask(event) {
+		if (event.key == "Enter" && this.state.newtask.length > 4) {
+			this.setState({
+				tasks: [...this.state.tasks, this.state.newtask],
+				newtask: ""
+			});
+		}
+	}
+
+	render() {
+		return (
+			<div className="text-center mt-5">
+				<h1>To-Do List</h1>
+
+				<input
+					value={this.state.newtask}
+					placeholder="New to-do"
+					onChange={event => this.handleChangeTask(event)}
+					onKeyPress={event => this.handleAddTask(event)}
+				/>
+				<ul>
+					{this.state.tasks.map((task, index) => {
+						return <li key={index}>{task}</li>;
+					})}
+				</ul>
+				<footer>Por realizar : {this.state.tasks.length}</footer>
+			</div>
+		);
+	}
 }
